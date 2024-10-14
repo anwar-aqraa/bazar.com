0de7521f73864b4ce5e8c9f95f827db0d1ace3b8
anwar_aqraa <anwaraqraa932@gmail.com>
2024-10-14 15:43:40 +0300

    .

--- catalog/catalog.js
+++ catalog/catalog.js
@@ -33,6 +33,7 @@ app.get('/search/:topic', (req, res) => {
 });
 
 // Get book info by item number
+
 app.get('/info/:item_number', (req, res) => {
     const { item_number } = req.params;
     db.get("SELECT title, stock, price,topic FROM books WHERE id = ?", [item_number], (err, row) => {
